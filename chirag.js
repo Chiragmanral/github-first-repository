615dda0494adc35c1050f4805594cf10f81d61e0
Chirag Manral <chiragmanral2003@gmail.com>
2022-10-01 11:25:35 +0530

    modify

--- chirag.js
+++ chirag.js
@@ -1,20 +1,22 @@
 alert("Here is you coder!!");
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+ HEAD
+ HEAD
 a=prompt("Enter your name :");
-<<<<<<< HEAD
-<<<<<<< HEAD
+ HEAD
+ HEAD
 alert("CR7 is a living legend.");
-=======
-=======
+
+
 alert("Here is chirag manral!!");
 alert(" CR7 ");
->>>>>>> software
->>>>>>> chirag
-=======
+ software
+ chirag
+
 b=prompt("Enter your class :");
->>>>>>> manral
-=======
+ manral
+
 alert("Genjutsu is the coolest..");
->>>>>>> prashant
+ prashant
+ 
+
